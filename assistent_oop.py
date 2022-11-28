6c6fa06bdb3641de67b36657b1c89c5ab74c567a
MaksymBratsiun <bratsyun_m@ukr.net>
2022-11-28 14:33:34 +0200

    worked

--- assistent_oop.py
+++ assistent_oop.py
@@ -1,9 +1,6 @@
 from collections import UserDict
 
 
-book = AddressBook()
-
-
 class Field:
     def __init__(self, name):
         Field.value = name
@@ -13,8 +10,11 @@ class Name(Field):
     pass
 
 
-class Phone(Field):
-    pass
+class Phone:
+    """при спробі наслідування Phone(Field) в обєктах класу Name(Field) при зміні self.phone змінювалося name.value """
+
+    def __init__(self, value):
+        self.phone = value
 
 
 class Record(Field):
@@ -24,29 +24,43 @@ class Record(Field):
 
     def add_phone(self, phone):
         self.phones.append(Phone(phone))
-        print('add complete')
 
     def change_phone(self, old_phone, new_phone):
-        for phone in self.phones:
-            if phone.value == old_phone:
-                self.add_phone(new_phone)
-                self.phones.remove(phone)
-                print('change complete')
+        if old_phone == new_phone:
+            return f'{old_phone} is the same number as {new_phone}'
+        else:
+            for phone in self.phones:
+                if phone.phone == old_phone:
+                    self.phones.append(Phone(new_phone))
+                    self.phones.pop(self.phones.index(phone))
 
     def delete_phone(self, delete_phone):
+        old_len = len(self.phones)
         for phone in self.phones:
-            if phone.value == delete_phone:
-                self.phones.remove(phone)
-                print('delete complete')
+            if phone.phone == delete_phone:
+                self.phones.pop(self.phones.index(phone))
+                print(f'delete {delete_phone} complete')
+        if len(self.phones) == old_len:
+            print(f'{delete_phone} not found')
 
 
 class AddressBook(UserDict):
 
     def add_record(self, record):
-        self.data[record.name.value] = record.phones
+        if record.name.value not in self.data:
+            self.data[record.name.value] = record.phones
+        else:
+            self.data[record.name.value].extend(record.phones)
+            set_phones = set(self.data[record.name.value])
+            self.data[record.name.value] = list(set_phones)
 
     def delete_contact(self, name):
         del self.data[name]
+
+
+""""""
+book = AddressBook()
+"""Create object AdressBook"""
 
 
 def input_error(func):  # decorator @input_error
@@ -61,28 +75,45 @@ def input_error(func):  # decorator @input_error
             result = "Phone number not entered."
         finally:
             return result
+
     return inner_function
 
 
 @input_error
 def add_handler(user_input):
     if len(user_input) >= 2:
-        phone_book.update({str(user_input[0]).title(): user_input[1]})
-    print(phone_book)
-    return f"{str(user_input[0]).title()}: {user_input[1]} succesfully added"
+        record = Record(user_input[0])
+        record.add_phone(user_input[1])
+        book.add_record(record)
+    return f"{str(user_input[0]).title()}: {user_input[1]} successfully added"
 
 
 @input_error
 def change_handler(user_input):
+    user_name = ''
     if len(user_input) >= 2:
-        phone_book.update({str(user_input[0]).title(): user_input[1]})
-    print(phone_book)
-    return f"{str(user_input[0]).title()}: {user_input[1]} succesfully changed"
+        for user in book:
+            if user_input[0] in [i.phone for i in book[user]]:
+                user_name = user
+                for phone in book[user]:
+                    if phone.phone == user_input[0]:
+                        book[user].append(Phone(user_input[1]))
+                        book[user].pop(book[user].index(phone))
+                        return f"Contact {user_name.title()}: old number {user_input[0]} " \
+                               f"successfully changed to {user_input[1]}"
+    if not user_name:
+        return f"Old number {user_input[0]} not found "
+
+
+@input_error
+def delete_handler(user_input):
+    if user_input[0]:
+        book.delete_contact(user_input[0])
+        return f"Contact {user_input[0].title()} deleted"
 
 
 def exit_handler(user_input):
-    print("Good bye!")
-    exit()
+    return "Good bye!"
 
 
 def error_handler(user_input):
@@ -97,7 +128,8 @@ def help_hendler(user_input):
     text = """ 
     "add" - add name(one word) and phone(digit): "add Ivan +380931234567"
     "close", "exit", "good bye" - exit from application: "exit"
-    "change" - change phone if name exists: "change Ivan +380937654321"
+    "change" - change old phone if exists to new phone: "change +380931234567 +380937654321"
+    "delete" - delete contact: "delete Ivan"
     "hello" -  print How can I help you?: "hello"
     "help": print help list: "help"
     "phone" - print phone of name: "phone Ivan"
@@ -128,15 +160,19 @@ def input_parser():
 @input_error
 def phone_handler(user_input):
     if len(user_input):
-        return f"{phone_book[str(user_input[0]).title()]}"
+        if user_input[0] in book:
+            phone_book_list = []
+            for phone in book[user_input[0]]:
+                phone_book_list.append(f"{user_input[0].title()}: {phone.phone}")
+            return "\n".join(phone_book_list)
     else:
         raise KeyError
 
 
 def show_handler(user_input):
     phone_book_list = []
-    for item in phone_book:
-        phone_book_list.append(f"{item}: {phone_book[item]}")
+    for name in book:
+        phone_book_list.append(f"{name.title()}: {[i.phone for i in book[name]]}")
     return "\n".join(phone_book_list)
 
 
@@ -149,7 +185,8 @@ HENDLER_DICT = {
     "hello": hello_handler,
     "help": help_hendler,
     "phone": phone_handler,
-    "show all": show_handler
+    "show all": show_handler,
+    "delete": delete_handler
 }
 
 
@@ -158,11 +195,12 @@ def get_handler(operator):
 
 
 def main():
-
     while True:
         user_input = input_parser()
-
-        print(get_handler(user_input[0])(user_input[1:]))
+        string = str(get_handler(user_input[0])(user_input[1:]))
+        print(string)
+        if string == "Good bye!":
+            exit()
 
 
 if __name__ == '__main__':
